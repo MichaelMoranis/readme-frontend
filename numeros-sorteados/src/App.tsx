230e6cec606b75091b6e854a91321ef6dae701a3
michaelmoranis <michaelmoranesul@gmail.com>
2024-08-31 16:08:14 -0300

    style: update title page root

--- numeros-sorteados/src/App.tsx
+++ numeros-sorteados/src/App.tsx
@@ -4,7 +4,7 @@ export function App() {
   return (
     <div className="text-center absolute top-52 max-h-screen text-black bg-zinc-100 flex w-full  p-6 rounded-t-[3rem]">
         <div className="flex flex-col gap-4 p-4">
-          <h1 className="font-montSerrat font-bold text-xl">Veja os números sorteios das loterias Caixa</h1>
+          <h1 className="font-montSerrat font-bold text-xl">Veja os números sorteados das loterias Caixa</h1>
           <img className="w-full rounded-full" src={bannerLoteria} alt="imagem da caixa" />
         </div>
     </div>
